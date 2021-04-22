387f8f136dc96e026554f86a139217ab8efcd046
prajnabhat111 <prajnabhat111@gmail.com>
2021-04-22 11:24:57 +0530

    added cards for formatting

--- app/src/main/java/com/example/foodgenicsmain/SearchAltAdapter.java
+++ app/src/main/java/com/example/foodgenicsmain/SearchAltAdapter.java
@@ -33,8 +33,8 @@ public class SearchAltAdapter extends RecyclerView.Adapter<SearchAltAdapter.View
         ViewHolder(View itemView) {
             super(itemView);
             ingred = itemView.findViewById(R.id.ingred);
-            respi = itemView.findViewById(R.id.res_name);
-            instr = itemView.findViewById(R.id.instruct);
+            instr = itemView.findViewById(R.id.res_name);
+            respi = itemView.findViewById(R.id.instruct);
         }
     }
     SearchAltAdapter(List<SearchResult> exampleList) {
